b78e7995a18e94afc27ebde4656807083ca94697
James Min <jmin499@gmail.com>
2020-09-25 03:51:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -22,7 +22,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+  return (age > 12 && age < 19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
