d61c3b7b24bbadb5808255840ecc373b6088e7f7
James Min <jmin499@gmail.com>
2020-09-25 03:41:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,18 +1,18 @@
 function basicTeenager(age) {
-  if(age > 13 && age < 19) {
-    return "You are a teenager"
-  }
+  // if(age > 13 && age < 19) {
+  //   return "You are a teenager"
+  // }
 }
 
 function teenager(age) {
-  if(age > 13 && age < 19) {
-    return "You are a teenager"
-  }
-  return "You are not a teenager"
+  // if(age > 13 && age < 19) {
+  //   return "You are a teenager"
+  // }
+  // return "You are not a teenager"
 }
 
 function ageChecker(age) {
-  
+
 }
 
 function ternaryTeenager(age) {
