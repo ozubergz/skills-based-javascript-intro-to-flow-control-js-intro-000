a4f999b400c5f08ba56eb2f6e00c3ffa83c1d9b7
James Min <jmin499@gmail.com>
2020-09-25 03:56:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,9 +28,9 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case 13:
-      return You are a teenager"
-    case 14:
-
+      return "You are a teenager"
+    default:
+      return ""
   }
 
 }
